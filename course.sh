6a030e40bf3ff04b34fdc5919ba15130ad9622cf
nimu <nimu@nnit.com>
2024-01-25 20:06:02 +0100

    Initial commit

---
+++ course.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+firefox http://www.physics.rutgers.edu/~haule/509/program.html
---
+++ youtube.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+firefox https://www.youtube.com/playlist?list=PLXmUYdQdC9IGv61Y1lhGBH0NsDQRdwcJE
